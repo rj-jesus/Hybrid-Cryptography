b7b9817d0c5c21960db46d9ba943b7558ebfe888
Ricardo Jesus <ricardojesus@ua.pt>
2015-03-15 22:43:16 +0000

    Appendiz rearranged

--- Python/KeysGenerator/generateKeys.py
+++ Python/KeysGenerator/generateKeys.py
@@ -6,7 +6,7 @@ keyPair = RSA.generate(1024)
 # For PrivateKey Generation
 
 f = open("priKey.pem", "w")
-f.write(keyPair.exportKey("PEM", "Code"))
+f.write(keyPair.exportKey("PEM", raw_input("Password to cipher key with:\n>>> ")))
 f.close()
 
 # For PublicKey Generation
